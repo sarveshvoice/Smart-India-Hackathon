4cbf1b0e480666bb627fbfb5c0e51d939f34f2b5
Deepak <raj.deeepak@gmail.com>
2017-04-01 23:04:52 +0530

    ADD : implememtation in Employer Section

--- app/src/main/java/com/mygov/parivartan/smartindiahackathon/employer/EmployerRegistration.java
+++ app/src/main/java/com/mygov/parivartan/smartindiahackathon/employer/EmployerRegistration.java
@@ -21,6 +21,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mygov.parivartan.smartindiahackathon.R;
 import com.mygov.parivartan.smartindiahackathon.employer.model.Employer;
 
 /**
